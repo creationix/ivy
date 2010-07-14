9cc5ca69fb43c7580f4d8c590a4d3649148bed9c
Tim Caswell <tim@creationix.com>
2010-07-14 14:01:25 -0700

    Update update.sh to use the git submodules.

--- utils/update.sh
+++ utils/update.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-BASE=`pwd`
-
-for name in `ls Modules`; do
-  cd "Modules/$name"
-  echo "Updating $name"
-  git pull origin master --tags
-  git submodule update
-  cd $BASE
-done
+git pull origin master --tags
+git submodule update
+./utils/relink.sh
